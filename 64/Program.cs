6c79e8740267f4bd6031cb7bbaddd3956d588373
aleksander <petrov_records@mail.ru>
2022-11-30 09:35:55 +0400

    64

---
+++ 64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2"
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int start = 1;
+Console.Write($"{EvenNumbers(n, start)} ");
+
+int EvenNumbers(int n, int start)
+{
+    if (n == start) return n;
+    if (start % 2 == 1)
+    {
+        return EvenNumbers(n, start + 1);
+    } 
+        Console.Write($"{EvenNumbers(n, start + 1)} ");
+        return start;
+}
+
+
